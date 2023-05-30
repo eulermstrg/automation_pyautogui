2ed83ee469a0f00126e6d1cafe13225915244d58
Euler Magno <eulermagno.mstrg@gmail.com>
2023-05-30 13:48:47 -0300

    Create bot_clicker_for_instagram.py
    bot to click platform-independent sequential commands. (example: instagram)

---
+++ Clicker/bot_clicker_for_istagram.py
@@ -0,0 +1,20 @@
+import pyautogui
+import time
+
+# Vide resources before
+
+# time.sleep(3)
+# print(pyautogui.position())
+
+
+# Range will be the number of posts you want to like
+for post in range(10):
+    pyautogui.moveTo(333, 474)
+    time.sleep(1)
+    pyautogui.doubleClick()
+    time.sleep(1)
+    pyautogui.moveTo(913, 573)
+    time.sleep(1)
+    pyautogui.leftClick()
+
+
